bfeb631b778e89ee64097113988ca2aa48f2c792
Artem Belkov <bestk1ngarthur@aol.com>
2021-11-17 22:40:27 +0300

    Add template for HW3

---
+++ Term 1/Java/HW3/src/main/java/ru/bestk1ng/java/hw3/App.java
@@ -0,0 +1,10 @@
+package ru.bestk1ng.java.hw3;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
